19161274967c1b9ad0c65e92952b2d9330a8be11
vinhHa28 <vinhhv28@gmail.com>
2025-05-29 22:58:27 +0700

    test ci cd when merge code

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Slideshow } from "./Slideshow";
 export { default as CateItem } from "./CateItem";
-export { default as ProductCard } from "./Slideshow";
+export { default as ProductCard } from "./ProductCard";
 export { default as FilterForm } from "./FilterForm";
